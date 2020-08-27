37ff2d35ab9b2fd17c98924a90f3b439c298be4e
Zisis Spatis <31807214+zisispa@users.noreply.github.com>
2020-08-27 21:54:30 +0300

    add ckecking the current user

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -17,4 +17,25 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-module.exports = { requireAuth };
+//check currect user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, "secret", async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await user.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
